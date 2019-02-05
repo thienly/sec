c2c931f3d3b0f7f45fdbb4f9c5d63ca7fff1dd24
Thien Ly <thien.ly@eton.vn>
2019-02-05 19:34:37 +0700

    First commit

---
+++ SagaState/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SagaState
+{
+    public class SagaDefinition
+    {
+        public SagaStatus Status { get; private set; }
+
+        public void SetStatus(SagaStatus status)
+        {
+            Status = status;
+        }
+    }
+
+    public class SagaStageDefinition
+    {
+        public SagaStageDefinition(IActivityDefinition trans, IActivityDefinition compensatingTrans)
+        {
+            Trans = trans;
+            CompensatingTrans = compensatingTrans;
+        }
+
+        public IActivityDefinition Trans { get; }
+        public IActivityDefinition CompensatingTrans { get; }
+    }
+
+    public interface IActivityDefinition
+    {
+
+    }
+
+    public enum SagaStatus
+    {
+        Created,
+        Started,
+        Aborted
+    }
+}
